643d119d69882495b975126f923b05db888c5631
Scott Wehby <scott.wehby@gmail.com>
2025-05-19 08:02:18 -0500

    disable debugging to increase performance.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,14 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            isDebuggable = false
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
